40e96e406d78fa7f933a2e01dbabed84264ee951
shuridadog <shuridadog@gmail.com>
2022-07-29 15:35:08 +0800

    edit config file

--- lib/config.ts
+++ lib/config.ts
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production'
 
-export const server = dev ? 'http://localhost:3000' : ''
+export const server = dev ? 'http://localhost:3000' : 'https://da-p2-task1-shuri.vercel.app'
